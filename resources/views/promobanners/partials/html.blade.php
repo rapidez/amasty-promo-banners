b1430e090a687df9aa6c2ed873df5522d8f63a03
Bob Wezelman <bob.wezelman@hotmail.com>
2021-08-03 11:53:57 +0200

    Support for banners on all categories

--- resources/views/promobanners/partials/html.blade.php
+++ resources/views/promobanners/partials/html.blade.php
@@ -2,7 +2,11 @@
     <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between flex-wrap">
             <div class="w-0 flex-1 flex items-center justify-between">
-                {!! $banner->html_text !!}
+                @if(str_contains($banner->html_text, 'widget'))
+                    Widgets not implemented yet.
+                @else
+                    {!! $banner->html_text !!}
+                @endif
             </div>
         </div>
     </div>
