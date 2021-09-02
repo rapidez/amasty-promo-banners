1de9b47c189e84eea9c7ad06a377bbf5ef303997
Bob Wezelman <bob.wezelman@hotmail.com>
2021-09-02 16:59:34 +0200

    Use content directive to support widgets (#3)

--- resources/views/promobanners/partials/html.blade.php
+++ resources/views/promobanners/partials/html.blade.php
@@ -2,11 +2,7 @@
     <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between flex-wrap">
             <div class="w-0 flex-1 flex items-center justify-between">
-                @if(str_contains($banner->html_text, 'widget'))
-                    Widgets not implemented yet.
-                @else
-                    {!! $banner->html_text !!}
-                @endif
+                @content($banner->html_text)
             </div>
         </div>
     </div>
