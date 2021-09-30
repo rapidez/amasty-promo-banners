42003b03bb1e9452851f894c1dc13692264eb512
Bob <bob@justbetter.nl>
2021-09-30 14:59:43 +0200

    Revert templates

--- resources/views/promobanners/partials/html.blade.php
+++ resources/views/promobanners/partials/html.blade.php
@@ -1,7 +1,4 @@
-@php
-$locations = [];
-@endphp
-<div class="bg-gray-200 rounded-lg my-4 {{ $locations[$location] ?? '' }}">
+<div class="bg-gray-200 rounded-lg my-4">
     <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between flex-wrap">
             <div class="w-0 flex-1 flex items-center justify-between">
--- resources/views/promobanners/partials/image.blade.php
+++ resources/views/promobanners/partials/image.blade.php
@@ -1,7 +1,4 @@
-@php
-$locations = [];
-@endphp
-<div class="h-20 my-4 {{ $locations[$location] ?? '' }}" style="background: url({{ config('rapidez.media_url') }}/amasty/ampromobanners/{{$banner->banner_img}})">
+<div class="h-20 my-4" style="background: url({{ config('rapidez.media_url') }}/amasty/ampromobanners/{{$banner->banner_img}})">
     <div class="max-w-7xl h-auto mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between flex-wrap">
             <div class="w-0 flex-1 flex items-center">
